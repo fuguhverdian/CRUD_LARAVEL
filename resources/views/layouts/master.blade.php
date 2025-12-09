75f499ea517a94b7ff20196938593068fa31743e
fuguhverdian <fahrulfuguhv@students.amikom.ac.id>
2025-12-10 03:15:49 +0700

    perubahan sedikit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Tokoku - Tutorial CRUD Laravel</title>
+        <title>CRUD Laravel sederhana</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
